5bd919cc177550b000bb5ab1c4835ab490926ad1
iliyastorm <theiliyastorm@gmail.com>
2022-11-04 17:45:01 +0330

    ساخت بوردر و پایان کار
    ساخت بوردر با استفاده از اچ تی ام ال
    تست و ران کردن پروژه و دیدن نتیجه
    مرتب کردن کد با استفاده از ریفورمت
    پایان پروژه

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
-<?php
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<table border="5">
 
-for ($a=1;$a<=5;$a++){
-    for ($b=1;$b<=5;$b++){
-        echo $a*$b;
+    <?php
 
+    for ($a = 1; $a <= 5; $a++) {
+        echo "<tr>";
+        for ($b = 1; $b <= 5; $b++) {
+            echo "<td>";
+            echo $a * $b;
+            echo "</td>";
+
+        }
+        echo "</br>";
+        echo "</tr>";
     }
-    echo "</br>";
-}
+    ?>
+
+</table>
+</body>
+</html>
